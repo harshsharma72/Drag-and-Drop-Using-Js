5f061fa8228c91fb02806f74e041cec4debd0b78
harshsharma72 <sharshsharma222@gmail.com>
2024-01-05 09:45:43 +0530

    final commit

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
 let lists = document.querySelectorAll(".list");
-let rightbox = document.querySelector(".right");
-let leftbox = document.querySelector(".left");
-for(list of lists){
-        list.addEventListener("dragstart", (e) => {
-            let selected = e.target;
-            
-        rightbox.addEventListener("dragover", (e) => {
-                e.preventDefault();
-            
-            
-        });
-        rightbox.addEventListener("drop", (e) => {
-            rightbox.appendChild(selected);
-            selected = null;
-        });
-        leftbox.addEventListener("dragover", (e) => {
-            e.preventDefault();
-        
-        
-        });
-        leftbox.addEventListener("drop", (e) => {
-            leftbox.appendChild(selected);
-            selected = null;
-        });
+let rightBox = document.querySelector("#right");
+let leftBox = document.querySelector("#left");
+for (list of lists) {
+  list.addEventListener("dragstart", (e) => {
+    let selected = e.target;
+    rightBox.addEventListener("dragover", (e) => {
+      e.preventDefault();
     });
-};
+    rightBox.addEventListener("drop", (e) => {
+      rightBox.appendChild(selected);
+      selected = null;
+    });
+    leftBox.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+    leftBox.addEventListener("drop", (e) => {
+      leftBox.appendChild(selected);
+      selected = null;
+    });
+  });
+}
